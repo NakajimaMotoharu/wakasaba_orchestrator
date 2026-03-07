bd971c7f962f1d79fec816ed6fdcf4ab0747a7f1
NakajimaMotoharu <mini.093421@gmail.com>
2026-03-07 19:12:48 +0900

    テストPUSH

--- src/main/java/com/wks/main/Main.java
+++ src/main/java/com/wks/main/Main.java
@@ -1,7 +1,10 @@
 package com.wks.main;
 
+import com.wks.util.SshExec;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello, WakasaNoTami");
+		SshExec.test();
 	}
 }
---
+++ src/main/java/com/wks/util/SshExec.java
@@ -0,0 +1,11 @@
+package com.wks.util;
+
+import com.jcraft.jsch.JSch;
+
+public class SshExec {
+	public static void test(){
+		JSch jSch = new JSch();
+
+		System.out.println("TEST" + jSch);
+	}
+}
